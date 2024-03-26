667c1cc5da5cf490dd8e5f0511a36be6ae2b1ecc
Ismail-Mokoena <ismailmokoena@icloud.com>
2024-03-26 01:59:49 -0500

    forward and backward pass

--- makemore/main.py
+++ makemore/main.py
@@ -1,6 +1,5 @@
 import torch
-
- 
+import torch.nn.functional as F
 
 def create_bi(words:list, letters)->torch.int32:
     N=torch.zeros((27,27), dtype=torch.int32)
@@ -15,6 +14,7 @@ def create_bi(words:list, letters)->torch.int32:
             ix1 = stoi[ch1]
             ix2 = stoi[ch2]
             N[ix1, ix2] += 1
+            #print(f'{ch1}{ch2}: {prob:}')
 
     return N
  
@@ -47,12 +47,63 @@ counts_array = create_bi(words, letters)
 #Probability of any single character being the first word
 #counts_array[0]=gives the counts of how often a character starts a word
 #first_row = counts_array[0].float()
+
+#probabilities = counts_array.float()
+#obeys  broadcasting rules
+#probabilities /= probabilities.sum(1,keepdim=True)
+
+#predictions(probabilities,g,letters)
+
+#cast into Neuralnet
+#We'll give our NN first char it will predict the next most likely char
+#1. create training set
+x_set, y_set = [], []
+#This iterates over all bigrams
+stoi = {s:i+1 for i,s in enumerate(letters)}
+#special token deliniate start/end
+stoi['.'] = 0
+for word in words[:1]:
+    print(word)
+    #special start token ane end token
+    tokenized = ['.'] + list(word) + ['.']
+    for ch1, ch2 in zip(tokenized, tokenized[1:]):
+        print(ch1,':',ch2)
+        x_set.append(stoi[ch1])
+        y_set.append(stoi[ch2])
+x_set = torch.tensor(x_set)
+y_set = torch.tensor(y_set)
+
+
+#weights
 #make deterministic generator object
 g = torch.Generator().manual_seed(2147483647)
-probabilities = counts_array.float()
-#obeys  broadcasting rules
-probabilities /= probabilities.sum(1,keepdim=True)
+weights = torch.randn((27,27), generator=g, requires_grad=True)
 
-predictions(probabilities,g,letters)
+##Gradient Descent
+for i in range(10):
+    ##Forward Pass
+    
+    #encoding using 1-hot-encoding
+    #then cast our vector to float~we feed floats into nn
+    x_encoded = F.one_hot(x_set,num_classes=27).float()
+
+    #@~ matrix multiplication opperator
+    logits  = x_encoded@weights
+
+    #counts equivalent to N
+    counts = (logits).exp() #x_encoded@weights log-counts
+    prob = counts/counts.sum(1, keepdims=True)
+
+    # Loss Function
+    #probability the model assignes to the next correct char
+    #neg log loss
+    loss = -prob[torch.arange(6), y_set].log().mean()
+    print(f'loss={loss}')
+    
+    ##Backward Pass
+    weights.grad = None
+    loss.backward()
+    #update tensor
+    weights.data += -0.1 * weights.grad
 
 
